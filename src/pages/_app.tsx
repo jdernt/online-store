3884c62644bc40943f9b44ca30aa2d57ed427c48
Natalia <jdernt@gmail.com>
2022-07-17 15:43:55 +0400

    fix: lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/components/normalize.css';
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
